532f0e854a93a1970c0acce6e7370621085bec44
Daniel Jordan <daniel.jordan@fellow-consulting.de>
2021-04-03 17:09:06 +0200

    next version

--- src/components/skills/mu/TextQA.tsx
+++ src/components/skills/mu/TextQA.tsx
@@ -40,13 +40,12 @@ const config: BaseSkillProps<Req, Res> = {
     The Sri Lankan constitution accords Buddhism the "foremost place", although it does not identify it as a state religion. Buddhism is given special privileges in the Sri Lankan constitution.`,
     question: 'What is SAARC?',
   }, {
-    text: `Su área de distribución comprende casi toda Sudamérica al este de los Andes en las cuencas del río Orinoco, del Amazonas y del Río de la Plata; cubriendo desde el este de Venezuela y la Guyana hasta Uruguay y el norte y centro de Argentina.
-    Pueden vivir en diferentes tipos de hábitat, pero muestran preferencia por algunos en concreto. Suelen encontrarse cerca de lagos, ríos, marismas o manglares.
-    También necesitan un suelo firme para dormir, idealmente con una vegetación espesa que les sirve de protección.
-    Para alimentarse no tienen problema en adentrarse por la sabana y herbazales.
-    La mayor densidad de población de carpinchos se encuentra en las extensas zonas húmedas de Sudamérica, como el Pantanal, o la región de los Llanos del norte del continente, bañada por el río Orinoco. Viven mayoritariamente en las llanuras, pero también habitan en altitudes de hasta 1300 metros por sobre el nivel del mar.
-    En comparación con otras especies animales de Sudamérica, las capibaras toleran bastante bien los cambios de hábitat provocados por la actividad humana, y también pueden sobrevivir en zonas transformadas en plantaciones o pastos.`,
-    question: '¿En qué países viven los capibaras?',
+    text: `Der Bundespräsident hat seine erste Impfung gegen Corona erhalten, und man könnte meinen, eine weitere Nebenwirkung des ominösen AstraZeneca-Vakzins sei politische Enthemmung. In seiner Fernsehansprache zum zweiten Ostern in der Pandemie wählt Frank-Walter Steinmeier, der des staatstragenden Schwurbelns sonst durchaus mächtig ist, deutliche Worte: Er konstatiert eine demokratische Vertrauenskrise, rügt die politisch Verantwortlichen für Fehler und fordert nach den jüngsten Querelen, sie sollten sich zusammenraufen. Es ist ein Rüffel, von dem sich die Kanzlerin genau so gemeint fühlen darf wie ihr längst in den Wahlkampfmodus übergetretener Koalitionspartner und sämtliche Ministerpräsidenten, mit Kanzlerambitionen oder ohne.
+
+    Frank-Walter Steinmeier hat sich, wie es die Tradition des Amtes will, selten in die Tagespolitik eingemischt: Einmal musste er, um aus einem Bundestagswahlergebnis auch eine Regierung entstehen zu lassen; einmal mahnte er auf dem Höhepunkt des Union-Streits um die Migration zur Mäßigung.
+    
+    Die Intervention jetzt ist besonders weitreichend, weil Steinmeier sich in einer Krise zu einem Anwalt der Bürger macht und sich mit ihnen über die Politik empört. Das ist legitim, aber ein schmaler Grat, auf dem schon Vorgänger wie Richard von Weizsäcker oder Horst Köhler nicht immer trittfest waren. Indem er das Ende seiner Rede nutzt, auch die Bürger in die Verantwortung für das große Ganze zu nehmen, tappt Steinmeier unterm Strich nicht in die Falle eines leichtfertigen Populismus. Aber stellenweise ist es ganz schön knapp.`,
+    question: 'Wer ist Steinmeier?',
   }, {
     text: `Su área de distribución comprende casi toda Sudamérica al este de los Andes en las cuencas del río Orinoco, del Amazonas y del Río de la Plata; cubriendo desde el este de Venezuela y la Guyana hasta Uruguay y el norte y centro de Argentina.
     Pueden vivir en diferentes tipos de hábitat, pero muestran preferencia por algunos en concreto. Suelen encontrarse cerca de lagos, ríos, marismas o manglares.
@@ -55,26 +54,7 @@ const config: BaseSkillProps<Req, Res> = {
     La mayor densidad de población de carpinchos se encuentra en las extensas zonas húmedas de Sudamérica, como el Pantanal, o la región de los Llanos del norte del continente, bañada por el río Orinoco. Viven mayoritariamente en las llanuras, pero también habitan en altitudes de hasta 1300 metros por sobre el nivel del mar.
     En comparación con otras especies animales de Sudamérica, las capibaras toleran bastante bien los cambios de hábitat provocados por la actividad humana, y también pueden sobrevivir en zonas transformadas en plantaciones o pastos.`,
     question: 'What countries do capybara live in?',
-  }, {
-    text: `Rosjanie zawsze odnosili wielkie sukcesy pod względem liczby utalentowanych sportowców i ilości zdobytych medali na igrzyskach olimpijskich oraz w innych zawodach międzynarodowych.
-    W przeciągu istnienia ZSRR radzieccy olimpijczycy zdobyli największą liczbę medali na 14 spośród 18 olimpiad.
-    Biorąc pod uwagę ten fakt, można stwierdzić, iż Związek Radziecki był w owym czasie dominującą potęgą sportową.
-    Począwszy od Olimpiady Letniej w 1952 r. sportowcy radzieccy zawsze byli w pierwszej trójce pod względem liczby zdobytych złotych medali. W 1980 r. Letnie Igrzyska Olimpijskie miały miejsce w Moskwie, a w 2014 r. Zimowe Igrzyska odbyły się w Soczi.
-    2 grudnia 2010 r. Komitet Wykonawczy FIFA powierzył Rosji organizację mistrzostw świata w piłce nożnej w 2018 r.`,
-    question: 'Gdzie odbyła się Olimpiada w 1980 roku?',
-  }, {
-    text: `Kirjasarjan ensimmäinen osa Harry Potter ja viisasten kivi (engl. Harry Potter and the Philosopher's Stone) ilmestyi alkuperäiskielellä vuonna 1997 ja sen viimeinen osa Harry Potter ja kuoleman varjelukset (engl. Harry Potter and the Deathly Hallows) vuonna 2007.
-    Ensimmäisen kirjan julkaisusta lähtien kirjasarja on saanut suurta suosiota ympäri maailmaa, ja se on ollut sekä arvostelu- että taloudellinen menestys. Kirjasarja on myynyt yli 500 miljoonaa kappaletta ja se on käännetty yli 73 kielelle.
-    Suomeksi Harry Potter -kirjasarjan kustansi Tammi ja sen käänsi Jaana Kapari-Jatta. Kirjasarja julkaistiin suomeksi vuosina 1998–2008.
-    Rowlingin tarinaan perustuva näytelmä, Harry Potter ja kirottu lapsi, sai ensi-iltansa Lontoossa 30. heinäkuuta 2016, ja sen käsikirjoitus julkaistiin myöhemmin kirjan muodossa.`,
-    question: 'Milloin kirjasarja julkaistiin suomeksi?',
-  }, {
-    text: `Kirjasarjan ensimmäinen osa Harry Potter ja viisasten kivi (engl. Harry Potter and the Philosopher's Stone) ilmestyi alkuperäiskielellä vuonna 1997 ja sen viimeinen osa Harry Potter ja kuoleman varjelukset (engl. Harry Potter and the Deathly Hallows) vuonna 2007.
-    Ensimmäisen kirjan julkaisusta lähtien kirjasarja on saanut suurta suosiota ympäri maailmaa, ja se on ollut sekä arvostelu- että taloudellinen menestys. Kirjasarja on myynyt yli 500 miljoonaa kappaletta ja se on käännetty yli 73 kielelle.
-    Suomeksi Harry Potter -kirjasarjan kustansi Tammi ja sen käänsi Jaana Kapari-Jatta. Kirjasarja julkaistiin suomeksi vuosina 1998–2008.
-    Rowlingin tarinaan perustuva näytelmä, Harry Potter ja kirottu lapsi, sai ensi-iltansa Lontoossa 30. heinäkuuta 2016, ja sen käsikirjoitus julkaistiin myöhemmin kirjan muodossa.`,
-    question: 'Как называется последняя книга про Гарри Поттера?',
-  }],
+  },],
   api: async (stateReq: Req) => {
     const req = {
       context_raw: [stateReq.text],
